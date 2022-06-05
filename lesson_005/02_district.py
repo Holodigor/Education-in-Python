d5783b58bfdc66bc824d8930182365df779d57c5
holod <igor.holod.s@gmail.com>
2022-06-05 19:11:06 +0300

    done

--- lesson_005/02_district.py
+++ lesson_005/02_district.py
@@ -5,15 +5,23 @@
 # подсказка: для вывода элементов списка через запятую можно использовать функцию строки .join()
 # https://docs.python.org/3/library/stdtypes.html#str.join
 
-import room_1
-import room_2
+import district.soviet_street.house1.room1
+import district.soviet_street.house1.room2
+import district.soviet_street.house2.room1
+import district.soviet_street.house2.room2
+import district.central_street.house1.room1
+import district.central_street.house1.room2
+import district.central_street.house2.room1
+import district.central_street.house2.room2
 
-people_list = room_1.folks + room_2.folks
+people_list = district.soviet_street.house1.room1.folks + \
+              district.soviet_street.house1.room2.folks + \
+              district.soviet_street.house2.room1.folks + \
+              district.soviet_street.house2.room2.folks + \
+              district.central_street.house1.room1.folks + \
+              district.central_street.house1.room2.folks + \
+              district.central_street.house2.room1.folks + \
+              district.central_street.house2.room2.folks
+
 people_string = ', '.join([i for i in people_list])
-print('На районе живут ',people_string)
-
-
-
-
-
-
+print('На районе живут ', people_string)
