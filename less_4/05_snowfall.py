da15d690f530469fcad96eda707e191d1bba4587
holod <igor.holod.s@gmail.com>
2022-06-05 13:48:59 +0300

    zad 5

--- less_4/05_snowfall.py
+++ less_4/05_snowfall.py
@@ -8,8 +8,6 @@ import simple_draw as sd
 # - нарисовать падение этих N снежинок
 # - создать список рандомных длинн лучей снежинок (от 10 до 100) и пусть все снежинки будут разные
 
-
-
 # Пригодятся функции
 # sd.get_point()
 # sd.snowflake()
@@ -18,21 +16,21 @@ import simple_draw as sd
 # sd.user_want_exit()
 
 
-def cristmas_snow(number_of_snowflakes=20):
+def christmas_snow(number_of_snowflakes=20):
     n = number_of_snowflakes
-    snow_centr_list = []
-    snow_lenght_list = []
+    snow_center_list = []
+    snow_length_list = []
     for i in range(n):
-        snow_centr_list.append(sd.get_point(sd.random_number(-200, 600), sd.random_number(500, 900)))
-        snow_lenght_list.append(sd.random_number(10, 50))
+        snow_center_list.append(sd.get_point(sd.random_number(-200, 600), sd.random_number(500, 900)))
+        snow_length_list.append(sd.random_number(10, 50))
     while True:
         sd.start_drawing()
-        for i, point in enumerate(snow_centr_list):
-            if point.y > snow_lenght_list[i] and point.x < 600:
-                sd.snowflake(center=point, length=snow_lenght_list[i])
+        for i, point in enumerate(snow_center_list):
+            if point.y > snow_length_list[i] and point.x < 600:
+                sd.snowflake(center=point, length=snow_length_list[i])
             else:
-                if point.y <= snow_lenght_list[i]:
-                    sd.snowflake(center=point, length=snow_lenght_list[i])
+                if point.y <= snow_length_list[i]:
+                    sd.snowflake(center=point, length=snow_length_list[i])
                 point.y = sd.random_number(600, 700)
                 point.x = sd.random_number(-30, 600)
         sd.finish_drawing()
@@ -40,14 +38,15 @@ def cristmas_snow(number_of_snowflakes=20):
         sd.sleep(0.05)
 
         sd.start_drawing()
-        for i, point in enumerate(snow_centr_list):
-            sd.snowflake(center=point, length=snow_lenght_list[i], color=sd.background_color)
+        for i, point in enumerate(snow_center_list):
+            sd.snowflake(center=point, length=snow_length_list[i], color=sd.background_color)
 
         sd.finish_drawing()
         if sd.user_want_exit():
             break
-        for i, snow in enumerate(snow_centr_list):
-            if snow_lenght_list[i] > 25:
+
+        for i, snow in enumerate(snow_center_list):
+            if snow_length_list[i] > 25:
                 speed = 4
                 acceleration = 2
             else:
@@ -57,7 +56,7 @@ def cristmas_snow(number_of_snowflakes=20):
             snow.x += acceleration + sd.random_number(-5, 5)
 
 
-cristmas_snow()
+christmas_snow()
 
 sd.pause()
 
