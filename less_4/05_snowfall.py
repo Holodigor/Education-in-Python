1055474134162116ef054047ee16836cdcc9f6e5
holod <igor.holod.s@gmail.com>
2022-06-05 13:34:24 +0300

    zad 5

--- less_4/05_snowfall.py
+++ less_4/05_snowfall.py
@@ -8,7 +8,7 @@ import simple_draw as sd
 # - нарисовать падение этих N снежинок
 # - создать список рандомных длинн лучей снежинок (от 10 до 100) и пусть все снежинки будут разные
 
-N = 20
+
 
 # Пригодятся функции
 # sd.get_point()
@@ -17,15 +17,47 @@ N = 20
 # sd.random_number()
 # sd.user_want_exit()
 
-# TODO здесь ваш код
-while True:
-    sd.clear_screen()
-    pass
-    pass
-    pass
-    sd.sleep(0.1)
-    if sd.user_want_exit():
-        break
+
+def cristmas_snow(number_of_snowflakes=20):
+    n = number_of_snowflakes
+    snow_centr_list = []
+    snow_lenght_list = []
+    for i in range(n):
+        snow_centr_list.append(sd.get_point(sd.random_number(-200, 600), sd.random_number(500, 900)))
+        snow_lenght_list.append(sd.random_number(10, 50))
+    while True:
+        sd.start_drawing()
+        for i, point in enumerate(snow_centr_list):
+            if point.y > snow_lenght_list[i] and point.x < 600:
+                sd.snowflake(center=point, length=snow_lenght_list[i])
+            else:
+                if point.y <= snow_lenght_list[i]:
+                    sd.snowflake(center=point, length=snow_lenght_list[i])
+                point.y = sd.random_number(600, 700)
+                point.x = sd.random_number(-30, 600)
+        sd.finish_drawing()
+
+        sd.sleep(0.05)
+
+        sd.start_drawing()
+        for i, point in enumerate(snow_centr_list):
+            sd.snowflake(center=point, length=snow_lenght_list[i], color=sd.background_color)
+
+        sd.finish_drawing()
+        if sd.user_want_exit():
+            break
+        for i, snow in enumerate(snow_centr_list):
+            if snow_lenght_list[i] > 25:
+                speed = 4
+                acceleration = 2
+            else:
+                speed = 8
+                acceleration = 1
+            snow.y -= speed
+            snow.x += acceleration + sd.random_number(-5, 5)
+
+
+cristmas_snow()
 
 sd.pause()
 
