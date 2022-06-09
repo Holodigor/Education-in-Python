6278a24b250270cfae2dadc81eb184098d4fd5df
holod <igor.holod.s@gmail.com>
2022-06-09 22:23:19 +0300

    02_alchemy

--- less_7/02_alchemy.py
+++ less_7/02_alchemy.py
@@ -19,8 +19,124 @@
 #   print(Water(), '+', Air(), '=', Water() + Air())
 #   print(Fire(), '+', Air(), '=', Fire() + Air())
 
-# TODO здесь ваш код
+class Air:
+    def __str__(self):
+        return f'Я воздух'
 
+    def __add__(self, other):
+        if isinstance(other, Water):
+            return Storm()
+        elif isinstance(other, Fire):
+            return Lightning()
+        elif isinstance(other, Earth):
+            return Dust()
+        else:
+            return None
+
+
+class Storm:
+    def __str__(self):
+        return f'Я шторм'
+
+    def __add__(self, other):
+        return None
+
+
+class Lightning:
+    def __str__(self):
+        return f'Я молния'
+
+    def __add__(self, other):
+        return None
+
+
+class Lava:
+    def __str__(self):
+        return f'Я лава'
+
+    def __add__(self, other):
+        return None
+
+
+class Fire:
+    def __str__(self):
+        return f'Я огонь'
+
+    def __add__(self, other):
+        if isinstance(other, Water):
+            return Steam()
+        elif isinstance(other, Air):
+            return Lightning()
+        elif isinstance(other, Earth):
+            return Lava()
+        else:
+            return None
+
+
+class Steam:
+    def __str__(self):
+        return f'Я пар'
+
+    def __add__(self, other):
+        return None
+
+
+class Dust:
+    def __str__(self):
+        return f'Я пиль'
+
+    def __add__(self, other):
+        return None
+
+
+class Earth:
+    def __str__(self):
+        return f'Я земля'
+
+    def __add__(self, other):
+        if isinstance(other, Water):
+            return Dirt()
+        elif isinstance(other, Air):
+            return Dust()
+        elif isinstance(other, Fire):
+            return Steam()
+        else:
+            return None
+
+
+class Dirt:
+    def __str__(self):
+        return f'Я грязь'
+
+    def __add__(self, other):
+        return None
+
+
+class Water:
+    def __str__(self):
+        return f'Я вода'
+
+    def __add__(self, other):
+        if isinstance(other, Air):
+            return Storm()
+        elif isinstance(other, Fire):
+            return Steam()
+        elif isinstance(other, Earth):
+            return Dirt()
+        else:
+            return None
+
+
+if __name__ == '__main__':
+    print(Water(), '+', Air(), '=', Water() + Air())
+    print(Fire(), '+', Air(), '=', Fire() + Air())
+    print(Fire(), '+', Water(), '=', Fire() + Water())
+    print(Fire(), '+', Earth(), '=', Fire() + Earth())
+    print(Air(), '+', Lightning(), '=', Air() + Lightning())
+    f = Fire()
+    w = Water()
+    res = f + w
+    print(res)
 # Усложненное задание (делать по желанию)
 # Добавить еще элемент в игру.
 # Придумать что будет при сложении существующих элементов с новым.
