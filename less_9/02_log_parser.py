c8d6d1596be52271c9688dc04beec5a239faece7
holod <igor.holod.s@gmail.com>
2022-06-20 17:59:30 +0300

    less 09 pars NOK

--- less_9/02_log_parser.py
+++ less_9/02_log_parser.py
@@ -18,9 +18,42 @@
 #
 # Входные параметры: файл для анализа, файл результата
 # Требования к коду: он должен быть готовым к расширению функциональности. Делать сразу на классах.
+import os
 
-# TODO здесь ваш код
 
+class AnalysisLogFile:
+
+    def __init__(self, log_file, rez_log_file='rez_log_file.txt'):
+        self.log_file = log_file
+        self.rez_log_file = rez_log_file
+
+    def _write_rez_log_file(self, line):
+        with open(self.rez_log_file, 'a') as rez_log_file:
+            rez_log_file.write(line)
+
+    def analysis_log_file(self):
+        os.remove(self.rez_log_file)
+        with open(self.log_file, 'r') as log_file:
+            count_NOK = 0
+            line_next = ''
+            for line in log_file.readlines():
+                if line.endswith('NOK\n'):
+                    if line_next == '':
+                        line_next = line[:-16]
+                        count_NOK += 1
+                    else:
+                        if line_next == line[:-16]:
+                            count_NOK += 1
+                        else:
+                            self._write_rez_log_file(line_next + '] ' + str(count_NOK) + '\n')
+                            line_next = line[:-16]
+                            count_NOK = 1
+        os.startfile(self.rez_log_file)
+
+
+if __name__ == '__main__':
+    log = AnalysisLogFile(log_file='events.txt')
+    log.analysis_log_file()
 # После выполнения первого этапа нужно сделать группировку событий
 #  - по часам
 #  - по месяцу
