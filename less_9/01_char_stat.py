576d2754cba29af045a75fc6f7283f665088c889
holod <igor.holod.s@gmail.com>
2022-06-20 16:57:12 +0300

    less 09 statistic and print refact

--- less_9/01_char_stat.py
+++ less_9/01_char_stat.py
@@ -22,6 +22,7 @@
 # Требования к коду: он должен быть готовым к расширению функциональности. Делать сразу на классах.
 import zipfile
 import os
+from abc import ABCMeta, abstractmethod
 
 
 class StatisticsLetters:
@@ -30,6 +31,7 @@ class StatisticsLetters:
         self.file = None
         self.file_name = file_name
         self.stat = {}
+        self.total_char = 0
 
     def search_path(self, path_dir='c:\\'):
         path = os.path.normpath(path_dir)
@@ -53,39 +55,88 @@ class StatisticsLetters:
                     if char.isalpha():
                         if char in self.stat:
                             self.stat[char] += line.count(char)
+                            self.total_char += 1
                         else:
                             self.stat[char] = 1
-
-    def sort_stat(self):
-        pass
+                            self.total_char += 1
 
     def write_in_file(self):
-        n = 0
         with open('rez.txt', 'w', encoding='utf8') as file:
-            file.write(f'+---------+----------+\n|{"Буква":^9}|{"Частота":^11}|\n+---------+----------+\n')
+            file.write(f'+---------+-----------+\n|{"Буква":^9}|{"Частота":^11}|\n+---------+-----------+\n')
             for char, count in self.stat.items():
-                n += count
                 file.write(f'|{char:^9}|{count:11d}|\n')
-            file.write(f'+---------+----------+\n|  итого  |{n:11d}|\n+---------+----------+')
+            file.write(f'+---------+----------+\n|  итого  |{self.total_char:11d}|\n+---------+-----------+')
+        os.startfile('rez.txt')
 
     def write_on_console(self):
-        n = 0
-        print(f'+---------+----------+\n|{"Буква":^9}|{"Частота":^11}|\n+---------+----------+\n', end='')
+        print(f'\n+---------+----------+\n|{"Буква":^9}|{"Частота":^11}|\n+---------+-----------+\n', end='')
         for char, count in self.stat.items():
-            n += count
             print(f'|{char:^9}|{count:11d}|\n', end='')
-        print(f'+---------+----------+\n|  итого  |{n:11d}|\n+---------+----------+', end='')
+        print(f'+---------+----------+\n|  итого  |{self.total_char:11d}|\n+---------+----------+', end='')
 
 
+class SortedDict(metaclass=ABCMeta):
+    def __init__(self, my_dict):
+        self.my_dict = my_dict
+        self.my_list = []
 
-top = 'C:\\Users\\holod\\PycharmProjects\\Education-in-Python\\less_9'
+    def _make_list(self):
+        for kay, value in self.my_dict.items():
+            self.my_list.append([kay, value])
+
+    def _make_dict(self):
+        self.my_dict = {}
+        for i in self.my_list:
+            if str(i[0]).isalpha():
+                self.my_dict[i[0]] = i[1]
+            else:
+                self.my_dict[i[1]] = i[0]
+
+    @abstractmethod
+    def sorted_dict(self):
+        pass
+
+
+class FrequencyAscending(SortedDict):  # по частоте по возрастанию
+
+    def sorted_dict(self):
+        self._make_list()
+        for i in self.my_list:
+            i[0], i[1] = i[1], i[0]
+        self.my_list = sorted(self.my_list)
+        self._make_dict()
+        return self.my_dict
+
+
+class AlphabeticallyAscending(SortedDict):  # - по алфавиту по возрастанию
+
+    def sorted_dict(self):
+        self._make_list()
+        self.my_list = sorted(self.my_list)
+        self._make_dict()
+        return self.my_dict
+
+
+class AlphabeticallyDescending(SortedDict):  # - по алфавиту по убыванию
+
+    def sorted_dict(self):
+        self._make_list()
+        self.my_list = sorted(self.my_list, reverse=True)
+        self._make_dict()
+        return self.my_dict
+
+
+top = r'C:\Users\holod\PycharmProjects\Education-in-Python\less_9'
 stat = StatisticsLetters('voyna-i-mir.txt.zip')
 stat.search_path(top)
 stat.statistic()
-stat.write_in_file()
-stat.sort_stat()
+stat.stat = AlphabeticallyDescending(stat.stat).sorted_dict()
 stat.write_on_console()
-
+stat.stat = AlphabeticallyAscending(stat.stat).sorted_dict()
+stat.write_on_console()
+stat.stat = FrequencyAscending(stat.stat).sorted_dict()
+stat.write_on_console()
+stat.write_in_file()
 # После выполнения первого этапа нужно сделать упорядочивание статистики
 #  - по частоте по возрастанию
 #  - по алфавиту по возрастанию
